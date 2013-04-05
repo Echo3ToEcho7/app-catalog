89087345b4db115821047121bb116edf5896b15a
Erika Brice and Kyle Morse <pair+ebrice+kmorse@rallydev.com>
2013-04-05 16:07:06 -0600

    Upgrade chart mashups to support IE9

--- portfoliokanban/PortfolioKanbanCard.js
+++ portfoliokanban/PortfolioKanbanCard.js
@@ -8,6 +8,8 @@
         extend:'Rally.ui.cardboard.Card',
         alias:'widget.rallyportfoliokanbancard',
 
+        //Delete this config when the ENABLE_SLIM_CARD_DESIGN
+        //toggle is removed so the ready functionality continues to be available
         config: {
             showIconsAndHighlightBorder: false
         },
