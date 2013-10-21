ab6c8cea1a6056c36cef18c89efef2a3757e8755
Dave Thompson <dthompson@rallydev.com>
2013-10-21 17:37:28 -0600

    Append appsdk version to catalog dist id.

--- grunt/tasks/version.js
+++ grunt/tasks/version.js
@@ -1,9 +1,28 @@
 module.exports = function(grunt) {
-    
+
+    var getSdkVersion = function() {
+        var js_dependencies = grunt.file.readJSON('js_dependencies.json'),
+            i, sdkVersion;
+
+        for (i = 0; i < js_dependencies.length; i++) {
+            var dep = js_dependencies[i];
+            if (dep.id.match(/appsdk-src/)) {
+                sdkVersion = dep.id.replace(/^.+:/, '');
+                break;
+            }
+        }
+
+        return sdkVersion;
+    };
+
+    var getBuildVersion = function(revision) {
+        var counter = process.env.PIPELINE_COUNTER || process.env.BUILD_NUMBER || 'dev';
+        return "" + counter + "-" + (revision.substr(0, 7)) + '-sdk-' + getSdkVersion();
+    };
+
     grunt.registerTask('version', 'Compute the build version for rally builds', function() {
         var done = this.async(), setVersion = function (revision) {
-            var counter = process.env.PIPELINE_COUNTER || process.env.BUILD_NUMBER || 'dev';
-            grunt.config(['buildVersion'], "" + counter + "-" + (revision.substr(0, 7)));
+            grunt.config(['buildVersion'], getBuildVersion(revision));
             done();
         };
         if (process.env.GIT_COMMIT) {
