15486519769478e9c7a3e980e906e06e14ceb1c7
Joel Sherriff <jsherriff@rallydev.com>
2013-10-25 08:56:09 -0400

    Revert "Merge pull request #12 from RallySoftware/S56042"
    This reverts commit ed9e9fe39770b09808896ec1923968e9de5d061a, reversing
    changes made to 6354e5c6ea4a106e8dc677f138dfc2f6cac9b9cb.

--- src/apps/charts/cfd/project/ProjectCFDApp.js
+++ src/apps/charts/cfd/project/ProjectCFDApp.js
@@ -197,21 +197,10 @@
             var find = {
                 '_TypeHierarchy': 'HierarchicalRequirement',
                 'Children': null,
-                "$or" :
-			        [
-                		{ '_ValidFrom': { "$gt": this._buildChartStoreConfigValidFrom() } },
-                		{
-                		  '_ValidTo'  : { "$gt" : this._buildChartStoreConfigValidFrom() },
-                		  '_ValidFrom': { "$lt": this._buildChartStoreConfigValidFrom() }
-                		}
-			        ]
+                '_ValidFrom': {
+                    "$gt": this._buildChartStoreConfigValidFrom()
+                }
             };
-            var stateField = this.getSetting('stateFieldName');
-            if(stateField === 'ScheduleState') {
-                find['$or'][1][stateField] = { "$lt": "Accepted" } ;
-            } else {
-                find['$or'][1][stateField] = { "$ne": this.getSetting('stateFieldValues').split(',').pop() } ;
-            }
 
             if (this.projectScopeDown) {
                 find._ProjectHierarchy = this.project.ObjectID;
@@ -247,11 +236,10 @@
         _buildChartCalculatorConfig: function() {
             var stateFieldName = this.getSetting('stateFieldName');
             var stateFieldValues = this.getSetting('stateFieldValues');
-            var startDate = this._buildChartStoreConfigValidFrom();
+
             return {
                 stateFieldName: stateFieldName,
-                stateFieldValues: stateFieldValues,
-                startDate: startDate
+                stateFieldValues: stateFieldValues
             };
         },
 
